36733557c09127e9baa32b791c168d4eb5e2e0e5
shjegal <shjegal@rockplace.co.kr>
2017-01-05 17:23:24 +0900

    test

---
+++ src/main/java/rock/SpringShjegalApplication.java
@@ -0,0 +1,12 @@
+package rock;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringShjegalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringShjegalApplication.class, args);
+	}
+}
